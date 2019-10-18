0629de110126a99455bf8cb5bfa1b95143dfc5bc
Riwajchalise <wazzree@gmail.com>
2019-10-18 17:54:00 +0545

    made some changes on CRUD

---
+++ CRUD project/README.md
@@ -0,0 +1,9 @@
+Simple Create, Read, Update, Delete (CRUD) in PHP & MySQL
+========
+
+A simple and basic system to add, edit, delete and view using PHP and MySQL. 
+
+Blog Article: [Very simple add, edit, delete, view in PHP & MySQL](http://blog.chapagain.com.np/very-simple-add-edit-delete-view-in-php-mysql/)
+
+SQL script to create database and tables is present in **database.sql** file.
+
---
+++ CRUD project/index.php
@@ -0,0 +1,38 @@
+<?php
+//including the database connection file
+include_once("config.php");
+
+//fetching data in descending order (lastest entry first)
+//$result = mysql_query("SELECT * FROM users ORDER BY id DESC"); // mysql_query is deprecated
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
+?>
+
+<html>
+<head>	
+	<title>Homepage</title>
+</head>
+
+<body>
+<a href="add.html">Add New Data</a><br/><br/>
+
+	<table width='80%' border=0>
+
+	<tr bgcolor='#CCCCCC'>
+		<td>Name</td>
+		<td>Age</td>
+		<td>Email</td>
+		<td>Update</td>
+	</tr>
+	<?php 
+	//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
+	while($res = mysqli_fetch_array($result)) { 		
+		echo "<tr>";
+		echo "<td>".$res['name']."</td>";
+		echo "<td>".$res['age']."</td>";
+		echo "<td>".$res['email']."</td>";	
+		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
+	}
+	?>
+	</table>
+</body>
+</html>
